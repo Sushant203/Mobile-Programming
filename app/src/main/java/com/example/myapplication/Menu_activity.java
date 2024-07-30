d7f7db743522029ee9cfa983af01afbddd835701
Sushant <clashofgames229@gmail.com>
2024-07-30 07:49:22 +0545

    added alert box in activity menu xml file

--- app/src/main/java/com/example/myapplication/Menu_activity.java
+++ app/src/main/java/com/example/myapplication/Menu_activity.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.ContextMenu;
@@ -8,9 +9,11 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.PopupMenu;
 import androidx.core.graphics.Insets;
@@ -18,13 +21,41 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Menu_activity extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener {
-
+AlertDialog.Builder builder;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
         Button btn= findViewById(R.id.button6);
         registerForContextMenu(btn);
+        Button alertbtn= findViewById(R.id.button7);
+        builder= new AlertDialog.Builder(this);
+        alertbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                builder.setMessage("are you sure you want to leave this site?").setCancelable(false).setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        finish();
+                        Toast.makeText(Menu_activity.this, "you choose yes action for alertbox", Toast.LENGTH_SHORT).show();
+                    }
+                })
+
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        //action for No button
+                        dialog.cancel();
+                        Toast.makeText(Menu_activity.this, "you choose no action for alertbox", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                //creating dailog box
+                AlertDialog alert= builder.create();
+                //setting title manually
+                alert.setTitle("AlertDialogExample");
+                alert.show();
+            }
+        });
     }
 
     @Override
