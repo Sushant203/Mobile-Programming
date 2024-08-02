4c0abd354717402de4d4103bfc74d948c605ad24
Sushant <clashofgames229@gmail.com>
2024-08-02 07:54:03 +0545

    list items made

---
+++ app/src/main/java/com/example/listapplication/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.listapplication;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView view = findViewById(R.id.listdetails);
+        String[] names = { "manoj", "sushant", "himal", "jivan" };
+
+        ArrayAdapter<String> namesAdapter = new ArrayAdapter<>(this, R.layout.list_item_layout, R.id.textlistitem, names);
+        view.setAdapter(namesAdapter);
+    }
+}
